88c53fa5b7b2db6b14212333ad031fe85267aa78
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2016-09-30 09:50:26 +0200

    Add arduino.org IDE

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -ti --rm --device=/dev/ttyUSB0 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/src/:/home/devel/src arduino
+docker run -ti --rm --device=/dev/ttyACM1 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/src/:/home/devel/src arduino
